16e59b2f6451a792a6f50060f9c7a83e71291a36
jcuartasl <jcuartasl@unal.edu.co>
2025-12-03 23:57:42 -0500

    finale2

--- homework/homework.py
+++ homework/homework.py
@@ -96,3 +96,138 @@
 # {'type': 'cm_matrix', 'dataset': 'train', 'true_0': {"predicted_0": 15562, "predicte_1": 666}, 'true_1': {"predicted_0": 3333, "predicted_1": 1444}}
 # {'type': 'cm_matrix', 'dataset': 'test', 'true_0': {"predicted_0": 15562, "predicte_1": 650}, 'true_1': {"predicted_0": 2490, "predicted_1": 1420}}
 #
+def load_data():
+    import pandas as pd
+
+    train = pd.read_csv("./files/input/train_data.csv.zip", compression="zip")
+    test = pd.read_csv("./files/input/test_data.csv.zip", compression="zip" )
+
+    return train, test
+
+def clean_data(data):
+    data = data.copy()
+    data.rename(columns = {'default payment next month':'default'}, inplace = True)
+    data.dropna(inplace=True)
+
+    data["EDUCATION"] = data["EDUCATION"].apply(lambda x: 5 if x not in [1,2,3,4] else x)
+    data.drop(columns=["ID"], inplace=True)
+
+    return data
+
+train_data, test_data = load_data()
+
+train_data = clean_data(train_data)
+test_data = clean_data(test_data)
+
+x_train = train_data.drop(columns=["default"])
+y_train = train_data["default"]
+x_test = test_data.drop(columns=["default"])
+y_test = test_data["default"]
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.neural_network import MLPClassifier
+from sklearn.pipeline import Pipeline
+
+def build_pipeline():
+    categorical_features = [col for col in x_train.columns if x_train[col].dtype == "object"]
+    numeric_features = [col for col in x_train.columns if x_train[col].dtype != "object"]
+    
+    onehot = ColumnTransformer(
+        transformers=[
+            ("cat", OneHotEncoder(handle_unknown="ignore"), categorical_features),
+            ("num", "passthrough", numeric_features),
+        ]
+    )
+
+    pipeline = Pipeline(steps=[
+        ("OneHotEncoder", onehot),
+        ("PCA", PCA(n_components=None)),
+        ("MinMaxScaler", MinMaxScaler()),
+        ("SelectKBest", SelectKBest(score_func=f_classif, k="all")),
+        ("MLPClassifier", MLPClassifier(max_iter=1000))
+    ])
+
+    return pipeline
+
+def optimize_params(pipeline):
+    from sklearn.model_selection import GridSearchCV
+    param_grid = {
+        "SelectKBest__k": ["all"],
+        # "SelectKBest__score_func": [f_classif],
+        # "MLPClassifier__hidden_layer_sizes": [(50,), (100,), (50, 50)],
+        # "MLPClassifier__solver": ["adam", "sgd"],
+        # "MLPClassifier__alpha": [0.0001, 0.001],
+        # "MLPClassifier__learning_rate": ["constant", "adaptive"]
+    }
+
+    grid_search = GridSearchCV(
+        estimator=pipeline,
+        param_grid=param_grid,
+        cv=10,
+        scoring="balanced_accuracy",
+    )
+
+    grid_search.fit(x_train, y_train)
+    return grid_search
+
+pipeline = build_pipeline()
+model = optimize_params(pipeline)
+
+def save_model(estimator):
+    import gzip
+    import pickle
+    import os
+
+    os.makedirs("./files/models", exist_ok=True)
+    with gzip.open("./files/models/model.pkl.gz", "wb") as f:
+        pickle.dump(estimator, f)
+save_model(model)
+
+def calc_metrics(estimator):
+    from sklearn.metrics import accuracy_score, balanced_accuracy_score, recall_score, f1_score
+    from sklearn.metrics import confusion_matrix
+    import os
+    import json
+
+    os.makedirs("./files/output", exist_ok=True)
+    with open("./files/output/metrics.json", "w") as f:
+        metrics_train = {
+            "dataset": "train",
+            "accuracy": accuracy_score(y_train, estimator.predict(x_train)),
+            "balanced_accuracy": balanced_accuracy_score(y_train, estimator.predict(x_train)),
+            "recall": recall_score(y_train, estimator.predict(x_train)),
+            "f1_score": f1_score(y_train, estimator.predict(x_train))
+        }
+        f.write(json.dumps(metrics_train) + "\n")
+
+        metrics_test = {
+            "dataset": "test",
+            "accuracy": accuracy_score(y_test, estimator.predict(x_test)),
+            "balanced_accuracy": balanced_accuracy_score(y_test, estimator.predict(x_test)),
+            "recall": recall_score(y_test, estimator.predict(x_test)),
+            "f1_score": f1_score(y_test, estimator.predict(x_test))
+        }
+        f.write(json.dumps(metrics_test) + "\n")
+
+        cm_matrix_train = confusion_matrix(y_train, estimator.predict(x_train))
+        cm_dict_train = {
+            "type": "cm_matrix",
+            "dataset": "train",
+            "true_0": {"predicted_0": int(cm_matrix_train[0,0]), "predicted_1": int(cm_matrix_train[0,1])},
+            "true_1": {"predicted_0": int(cm_matrix_train[1,0]), "predicted_1": int(cm_matrix_train[1,1])}
+        }
+        f.write(json.dumps(cm_dict_train) + "\n")
+
+        cm_matrix_test = confusion_matrix(y_test, estimator.predict(x_test))
+        cm_dict_test = {
+            "type": "cm_matrix",
+            "dataset": "test",
+            "true_0": {"predicted_0": int(cm_matrix_test[0,0]), "predicted_1": int(cm_matrix_test[0,1])},
+            "true_1": {"predicted_0": int(cm_matrix_test[1,0]), "predicted_1": int(cm_matrix_test[1,1])}
+        }
+        f.write(json.dumps(cm_dict_test) + "\n")
+
+calc_metrics(model)
